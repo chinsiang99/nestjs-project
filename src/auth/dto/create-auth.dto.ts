955c25bb60ceba417ef643f0472db72ba16e2db1
chinsiang99 <chinsiang99@gmail.com>
2024-04-29 16:40:55 +0800

    refactor: refactor register dto swagger

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -34,6 +34,7 @@ export class RegisterDto {
   // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, { message: 'Password must contain at least 8 characters, including uppercase, lowercase, numbers, and special characters.' })
   password: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @IsPasswordMatching('password')
