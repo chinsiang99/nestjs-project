10e0eb869ed127c6acbcef108adbdb6d145d54a7
chinsiang99 <chinsiang99@gmail.com>
2024-04-29 14:45:29 +0800

    feat: create user entity

---
+++ src/auth/entities/auth.entity.ts
@@ -0,0 +1,28 @@
+import { AuthStrategyEnum } from '../../utils/enums/auth-strategy.enum';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    length: 255,
+    nullable: false,
+    name: 'email',
+  })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false, name: 'password' })
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: AuthStrategyEnum,
+    default: AuthStrategyEnum.SYSTEM,
+    name: 'auth_strategy',
+  })
+  authStrategy: AuthStrategyEnum = AuthStrategyEnum.SYSTEM;
+}
